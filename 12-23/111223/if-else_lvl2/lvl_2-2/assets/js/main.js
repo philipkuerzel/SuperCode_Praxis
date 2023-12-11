c9e4d228582c0151f3fe47bb955f5cb3ec48707d
philipkuerzel <philipkuerzel@gmail.com>
2023-12-11 15:45:40 +0100

    lvl_2-2

--- 12-23/111223/if-else_lvl2/lvl_2-2/assets/js/main.js
+++ 12-23/111223/if-else_lvl2/lvl_2-2/assets/js/main.js
@@ -1,3 +1,17 @@
-function checkAirQuality(){
-    
+function checkAirQuality() {
+    let airQuality = document.getElementById("airQuality").value;
+    const message = document.getElementById("message")
+    if (airQuality <= 50) {
+        document.body.style.backgroundColor = "#00e400";
+        message.textContent = "Level of health concern: Good Level of health effect Little or no risk"
+        
+    } 
+    else if (airQuality >50 && airQuality <= 100) {
+        document.body.style.backgroundColor = "#ffff00"; 
+        message.textContent = "Level of health concern: Moderate Level of health effect: Acceptable quality"
+    } 
+    else if (airQuality >100 && airQuality <= 150) {
+        document.body.style.backgroundColor = "#e40000";
+        message.textContent = "Unhealthy for sensitive groups. Level of health effect: Generable publics not likely affected"
+    }
 }
