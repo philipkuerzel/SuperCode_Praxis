56c9918c173cf45bcf985ec09f1a6c64022055b7
philipkuerzel <philipkuerzel@gmail.com>
2023-12-11 13:15:50 +0100

    logical operator

---
+++ 12-23/111223/logical_operator/assets/js/main.js
@@ -0,0 +1,10 @@
+let x = 10
+let y = 15
+let z = 20
+
+console.log(x > z && x > y);
+console.log(x != y);
+console.log(z < y || z > x);
+console.log(x === z || x != y);
+console.log(x >= 10 && y <= 10);
+console.log(x * z < 100|| x * y > 100);
