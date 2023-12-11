d5a821442eed42c38df48437cb03417e7fef64c9
philipkuerzel <philipkuerzel@gmail.com>
2023-12-11 14:51:30 +0100

    lvl_2-3

---
+++ 12-23/111223/if-else_lvl2/lvl_2-3/assets/js/main.js
@@ -0,0 +1,10 @@
+function multiDiff(num){
+    let difference = num - 27
+    if (num > 27){
+        newNum = difference * 2
+    }
+    console.log(newNum);
+}
+multiDiff(35)
+multiDiff(74)
+multiDiff(123)
