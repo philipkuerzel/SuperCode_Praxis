769d3b46bbeec143453fd1187e3780aa5fa2263d
philipkuerzel <philipkuerzel@gmail.com>
2023-12-11 16:32:23 +0100

    lvl_2-2

--- 12-23/111223/if-else_lvl2/lvl_2-2/assets/js/main.js
+++ 12-23/111223/if-else_lvl2/lvl_2-2/assets/js/main.js
@@ -1,17 +1,26 @@
 function checkAirQuality() {
     let airQuality = document.getElementById("airQuality").value;
-    const message = document.getElementById("message")
+    let qualityNumber = document.querySelector("span")
+    const message1 = document.getElementById("message1")
+    const message2 = document.getElementById("message2")
+
     if (airQuality <= 50) {
+        qualityNumber.innerHTML = `${airQuality}`
         document.body.style.backgroundColor = "#00e400";
-        message.textContent = "Level of health concern: Good Level of health effect Little or no risk"
+        message1.textContent = "Level of health concern: Good"
+        message2.textContent = "Level of health effect Little or no risk"
         
     } 
     else if (airQuality >50 && airQuality <= 100) {
+        qualityNumber.innerHTML = `${airQuality}`
         document.body.style.backgroundColor = "#ffff00"; 
-        message.textContent = "Level of health concern: Moderate Level of health effect: Acceptable quality"
+        message1.textContent = "Level of health concern: Moderate"
+        message2.textContent = "Level of health effect: Acceptable quality"
     } 
     else if (airQuality >100 && airQuality <= 150) {
+        qualityNumber.innerHTML = `${airQuality}`
         document.body.style.backgroundColor = "#e40000";
-        message.textContent = "Unhealthy for sensitive groups. Level of health effect: Generable publics not likely affected"
+        message1.textContent = "Level of Health: Unhealthy for sensitive groups"
+        message2.textContent = "Level of health effect: Generable publics not likely affected"
     }
 }
