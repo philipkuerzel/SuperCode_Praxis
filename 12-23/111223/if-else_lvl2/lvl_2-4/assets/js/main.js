2824028f2da18fd24c2e4b505b0028728eb8c2c6
philipkuerzel <philipkuerzel@gmail.com>
2023-12-11 14:56:23 +0100

    lvl_2-4

---
+++ 12-23/111223/if-else_lvl2/lvl_2-4/assets/js/main.js
@@ -0,0 +1,10 @@
+function multiSum(num1,num2){
+    let sum = num1 + num2
+    if (num1 == num2){
+        console.log(sum*5);
+    }
+    else {
+        console.log(sum);
+    }
+}
+multiSum(12,12)
