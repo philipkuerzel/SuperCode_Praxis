078cd0d2047cf9cd60b3618f9c84a4e668c0c31b
philipkuerzel <philipkuerzel@gmail.com>
2023-12-14 11:21:53 +0100

    lvl1-9

---
+++ 12-23/141223/func_1-9/assets/js/main.js
@@ -0,0 +1,6 @@
+const aboutMe = (vorname, nachname, geburtsort, alter, wohnort) => {
+    console.log("Mein Name ist " + vorname +" "+ nachname + "." + "Ich bin in " + geburtsort + "geboren. Ich bin " + alter + " Jahre alt. Ich wohne in " + wohnort + "."); }
+
+    aboutMe("Paul", "Panzer", "Entenhausen", "55", "Los Angeles")
+    
+
