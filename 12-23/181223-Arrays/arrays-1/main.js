8ebdb3955f9968d695ac4a89c71aacf4e6a2d5f3
philipkuerzel <philipkuerzel@gmail.com>
2023-12-18 12:37:11 +0100

    array_1-1

---
+++ 12-23/181223-Arrays/arrays-1/main.js
@@ -0,0 +1,5 @@
+let infoPerson = ["Dagobert", "Duck", "Reich"]
+let infoFriends = ["Tick", "Trick", "Track"]
+let infoFood = ["Burger", "Sushi", "Pizza"]
+
+console.log(infoPerson, infoFriends, infoFood);
