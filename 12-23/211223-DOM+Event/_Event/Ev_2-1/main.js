7489b54bfa72fba380da42e347a16236a73f048e
philipkuerzel <philipkuerzel@gmail.com>
2023-12-21 17:13:56 +0100

    event_2-1 done

---
+++ 12-23/211223-DOM+Event/_Event/Ev_2-1/main.js
@@ -0,0 +1,22 @@
+const grey = document.getElementById("grauTaste")
+grey.addEventListener("click", (event) => {
+    document.body.style.backgroundColor = "grey";
+});
+
+const white = document.getElementById("weissTaste")
+white.addEventListener("click", (event) => {
+    document.body.style.backgroundColor = "white";
+});
+
+const blue = document.getElementById("blauTaste")
+blue.addEventListener("click", (event) => {
+    document.body.style.backgroundColor = "blue";
+});
+
+const yellow = document.getElementById("gelbTaste")
+yellow.addEventListener("click", (event) => {
+    document.body.style.backgroundColor = "yellow";
+});
+
+
+
