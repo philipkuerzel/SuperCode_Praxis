cd4a65025d2e2665566fee4a43fedd3d00eb8e8c
philipkuerzel <philipkuerzel@gmail.com>
2024-01-02 17:49:07 +0100

    this 1-4

--- 01-24/objects_2/main.js
+++ 01-24/objects_2/main.js
@@ -12,7 +12,7 @@ let person = {
 console.log(person.fullName());
 
 // this 1-2
-console.log("%c this 1-2", "color: red");
+console.log("%c this 1-2 siehe HTML", "color: red");
 
 let person2 = {
     vorname: 'Anton',
@@ -33,3 +33,37 @@ let person2 = {
 };
 
 document.getElementById("person").innerHTML = person2.profil();
+
+
+// this 1-3
+console.log("%c this 1-3 siehe HTML", "color: red");
+
+let personFunction = {
+    fullName: function () {
+        return this.vorname + " " + this.nachname;
+    }
+};
+let personObjekt = {
+    vorname: "Anton",
+    nachname: "Fish"
+};
+
+let anzeigeImHTMLDokument = `<p>${personFunction.fullName.call(personObjekt)}</p>`;
+document.getElementById("person").innerHTML = anzeigeImHTMLDokument;
+
+
+
+// this 1-4
+console.log("%c this 1-4", "color: red");
+
+function fahrrad() {
+    console.log(this.name);
+}
+
+let name = "Canyon";
+let obj1 = { name: "Merida", fahrrad: fahrrad };
+let obj2 = { name: "Scott", fahrrad: fahrrad };
+
+this.fahrrad(); // leer
+obj1.fahrrad(); // Merida
+obj2.fahrrad(); // Scott
