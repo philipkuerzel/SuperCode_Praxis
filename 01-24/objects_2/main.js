31f181c46fb45b8b1db3d304fdd785eef32a4023
philipkuerzel <philipkuerzel@gmail.com>
2024-01-02 17:40:07 +0100

    this 1-2

--- 01-24/objects_2/main.js
+++ 01-24/objects_2/main.js
@@ -11,3 +11,25 @@ let person = {
 
 console.log(person.fullName());
 
+// this 1-2
+console.log("%c this 1-2", "color: red");
+
+let person2 = {
+    vorname: 'Anton',
+    nachname: 'Fish',
+    alter: 34,
+    familienstand: 'ledig',
+    groesse: '178cm',
+    profil: function () {
+        return(
+            `
+            <p>Name: ${this.vorname} ${this.nachname}<br>
+            Alter: ${this.alter} Jahre <br>
+            Körpergröße: ${this.groesse} <br>
+            Familienstand: ${this.familienstand}</p>
+            `
+        );
+}
+};
+
+document.getElementById("person").innerHTML = person2.profil();
