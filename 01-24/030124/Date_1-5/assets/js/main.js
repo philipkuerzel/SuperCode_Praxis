117ad080f3d34b23c7b05a0ca9fc1bbdc5776ed6
philipkuerzel <philipkuerzel@gmail.com>
2024-01-03 13:06:33 +0100

    date 1-5 done

---
+++ 01-24/030124/Date_1-5/assets/js/main.js
@@ -0,0 +1,20 @@
+let list = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+];
+
+const monatsName = (month) => list[new Date(month).getMonth()]
+
+console.log(monatsName("2001-3-4")) // März
+console.log(monatsName("2019-12-24")) // Dezember
+console.log(monatsName("1410-07-15")) // Juli
