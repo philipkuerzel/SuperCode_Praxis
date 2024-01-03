d193b66262a687f71ea70a8bec1256c87000d187
philipkuerzel <philipkuerzel@gmail.com>
2024-01-03 13:36:04 +0100

    änderung date 1-7

--- 01-24/030124/Date_1-7/assets/js/main.js
+++ 01-24/030124/Date_1-7/assets/js/main.js
@@ -1,11 +1,5 @@
-const checkDay = (day) => {
-    const d = new Date(day).getDay()
-    if (d % 6 !== 0){  // 1-5 --> Montag - Freitag
-        return "Arbeitstag"
-    }else if (d % 6 === 0){  // 0 + 6 --> Sonntag und Samstag
-        return "Weekend"
-    }
-}
+const checkDay = (day) => new Date(day).getDay() % 6 !== 0 ? "Arbeitstag" : "Weekend";
+
 console.log(checkDay("12.15.2019")); // Weekend
 console.log(checkDay("2.16.2001")); // Arbeitstag
 console.log(checkDay("2.1.2020")); // Weekend
