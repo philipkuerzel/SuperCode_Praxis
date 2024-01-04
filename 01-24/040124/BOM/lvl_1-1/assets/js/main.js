f4e831a9eea026b2127f8691575e00e4218fe9cd
philipkuerzel <philipkuerzel@gmail.com>
2024-01-04 11:46:34 +0100

    Ordner erstellt 040124

---
+++ 01-24/040124/BOM/lvl_1-1/assets/js/main.js
@@ -0,0 +1,16 @@
+console.log("Start: Warten für 3 Sekunden");
+
+const firstThreeSeconds = setTimeout(() => {
+    console.log("Erledigt. Du hast 3 Sekunden verschwendet!");
+}, 3000)
+
+let counter = 10
+const countingToZero = setInterval(() => {
+    if (counter > 0){
+        console.log(counter);
+        counter--
+    }else {
+        console.log("Endlich Feierabend!");
+        clearInterval(countingToZero)
+    }
+},1000)
