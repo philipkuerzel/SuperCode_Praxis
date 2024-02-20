18b0ea4ba475d77b7d123bc005935624d4ea3848
philipkuerzel <philipkuerzel@gmail.com>
2024-02-20 14:55:36 +0100

    lvl 1-2 done

--- Backend/02-24/impExp_lvl1-2/index.js
+++ Backend/02-24/impExp_lvl1-2/index.js
@@ -1,14 +1,14 @@
 import {names, numbers} from './data.js'
 import { firstElement,arrWithoutFirstElement,arrWithoutLastElement, lastElement, removeElement, removeDuplicates, sumOfArray, randomNumberBetween, capitalizeFirstLetter, capitalize, lastLetter} from './functions.js'
 
-console.log(firstElement(names));
-console.log(lastElement(names));
-console.log(arrWithoutFirstElement(names));
-console.log(arrWithoutLastElement(names));
-console.log(removeElement(numbers, 55));
-console.log(removeDuplicates(numbers));
-console.log(sumOfArray(numbers));
-console.log(randomNumberBetween(9, 77));
-console.log(capitalizeFirstLetter("javascript"));
-console.log(capitalize("javascript"));
-console.log(lastLetter("javascript", "p"));
+console.log("first element: ",firstElement(names));
+console.log("last element: ",lastElement(names));
+console.log("array without first element: ",arrWithoutFirstElement(names));
+console.log("array without last element: ",arrWithoutLastElement(names));
+console.log("remove specific element: ",removeElement(numbers, 55));
+console.log("remove duplicates: ",removeDuplicates(numbers));
+console.log("sum of numbers: ",sumOfArray(numbers));
+console.log("random number between: ",randomNumberBetween(9, 77));
+console.log("capitalize first letter: ",capitalizeFirstLetter("javascript"));
+console.log("capitalize string: ",capitalize("javascript"));
+console.log("compare last letter:",lastLetter("javascript", "p"));
