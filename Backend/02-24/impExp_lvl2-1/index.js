e9513daecad0cbf6c5600c7e4fa3bb20da36cf49
philipkuerzel <philipkuerzel@gmail.com>
2024-02-20 14:51:39 +0100

    lvl 2-1 done

--- Backend/02-24/impExp_lvl2-1/index.js
+++ Backend/02-24/impExp_lvl2-1/index.js
@@ -1,5 +1,5 @@
 import { cities } from "./cities.js";
 import { popOver100, popUnder100 } from "./functions.js";
 
-console.log(popOver100(cities));
-console.log(popUnder100(cities));
+console.log("population > 100k:" , popOver100(cities));
+console.log("population < 100k:",popUnder100(cities));
