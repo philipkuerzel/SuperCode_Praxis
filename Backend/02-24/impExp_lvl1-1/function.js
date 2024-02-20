5ae8b918bc1504b64a86929a12d71477912775b4
philipkuerzel <philipkuerzel@gmail.com>
2024-02-20 14:26:43 +0100

    lvl 1-1 done

---
+++ Backend/02-24/impExp_lvl1-1/function.js
@@ -0,0 +1,5 @@
+const sortArray = (arr) => {
+    return arr.sort((a, b) => a - b );
+}
+
+export default sortArray; // This is the default export
---
+++ Backend/02-24/impExp_lvl1-1/index.js
@@ -0,0 +1,5 @@
+import sortArray from "./function.js";
+import { arrayCars, arrayNumbers } from "./data.js";
+
+console.log(sortArray(arrayCars))
+console.log(sortArray(arrayNumbers));
