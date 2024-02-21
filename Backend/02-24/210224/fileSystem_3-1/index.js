9d0c382178ae93243b06530cb381516c60ae9802
philipkuerzel <philipkuerzel@gmail.com>
2024-02-21 17:10:54 +0100

    lvl 3-1 done

---
+++ Backend/02-24/210224/fileSystem_3-1/index.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+
+
+const checkFile = (text) => {
+    fs.access("./unterordner", fs.constants.F_OK, (err) => {
+        // check if the subfolder exists
+        if (err) {
+            // create subfolder if it doesn´t exist
+            fs.mkdir("./unterordner", (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }console.log("Folder created");
+                // write text to file, take (parameter) and add a new line
+                fs.writeFile("./unterordner/file.txt", text + "\n", (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log("File created and text written!");
+                });
+            });
+        } else {
+            // if subfolder exists, write text to the file
+            fs.writeFile("./unterordner/file.txt", text + "\n", { flag: "a" }, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log("Text added to the existing file!");
+            });
+        }
+    });
+};
+
+checkFile("tests ohne ende")
