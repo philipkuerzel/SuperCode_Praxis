1a75a66b1b4cf3742dc297086e1c9171ec3eab63
philipkuerzel <philipkuerzel@gmail.com>
2024-02-23 15:09:15 +0100

    3-1 done

---
+++ Backend/02-24/230224/promises_3-1/index.js
@@ -0,0 +1,26 @@
+const multiply = (number) => {
+    return new Promise((resolve, reject) => {
+        if (isNaN(number) === false) {
+        resolve(number * number);
+        } else {
+        reject('please enter a number');
+        }
+    });
+}
+
+const multiplyThreeTimes = (number) => {
+    return multiply(number)
+    .then((result) => {
+        return multiply(result);
+    })
+    .then((result) => {
+        return multiply(result);
+    });
+}
+
+multiplyThreeTimes(3)
+.then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+})
