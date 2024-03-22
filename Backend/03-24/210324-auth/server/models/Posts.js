c31a49549d1e3f443eef6f223de40045353d98c0
philipkuerzel <145387977+philipkuerzel@users.noreply.github.com>
2024-03-22 10:13:38 +0100

    Delete Backend/03-24/210324-auth/server directory

--- Backend/03-24/210324-auth/server/models/Posts.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const postSchema = new mongoose.Schema({
-  body: {type: String, required: true},
-  createdAt: {type: Date, default: Date.now},
-  user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
-})
-
-export const Post = mongoose.model("Post", postSchema);
